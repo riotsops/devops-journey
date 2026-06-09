7556d744b60ebfb36ef847ff0df48d519b5837d5
riotsops <karanbantri2@gmail.com>
2026-06-09 18:25:11 +0530

    add morning automation script

---
+++ morning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Day started!"
+echo "Checking devops-journey folder..."
+ls ~/devops-journey
+echo "Git status:"
+git -C ~/devops-journey status
+echo "Ready to learn!"
+echo "Current date and time:"
+date
+
+
