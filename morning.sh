8d213a449fda9bf3d76a68cc6b881fbb36232c5e
riotsops <karanbantri2@gmail.com>
2026-06-10 14:56:38 +0530

    Add time-based greeting with morning/afternoon/evening

--- morning.sh
+++ morning.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+PROJECT=~/devops-journey
+NAME="riotsops"
 echo "Day started!"
 echo "Checking devops-journey folder..."
-ls ~/devops-journey
+ls $PROJECT
 echo "Git status:"
 git -C ~/devops-journey status
 echo "Ready to learn!"
 echo "Current date and time:"
 date
+echo "Learning DevOps, $NAME!"
 
-
+HOUR=$(date +%H);
+if [ $HOUR -lt 12 ]; then 
+  echo "Good Morning, Have a nice day"
+elif [ $HOUR -gt 12 -a $HOUR -lt 17 ]; then
+ echo "good afternoon"
+else
+  echo "Good evening, Hope your day went good"
+fi
