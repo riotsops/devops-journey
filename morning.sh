17b5df2a9cab67da0d52044bfead7b28cef054cc
riotsops <karanbantri2@gmail.com>
2026-06-11 08:48:29 +0530

    Complete morning.sh - add CPU health check

--- morning.sh
+++ morning.sh
@@ -24,13 +24,21 @@ DISK=$(df /storage/emulated/0 | tail -1 | awk {'print $5'} | tr -d '%')
 if [ $DISK -gt 90 ]; then
  echo "storage is about to full - clean it"
 else
- echo "storage is fine"
+ echo "storage is fine" 
 fi
 
-MEM=$(free | grep Mem | awk {'printf "%d", $3/$2'})
+MEM=$(free | grep Mem | awk {'printf "%d", $3/$2 * 100'})
 if [ $MEM -gt 90 ]; then
  echo " ram is running out "
 else
  echo " Ram is good "
+fi
+
+CPU_IDLE=$(top -bn1 | grep "cpu" | awk {'print $5'} | grep -o '[0-9]*' | head -1)
+CPU_IDLE=$((CPU_IDLE /8))
+if [ $CPU_IDLE -lt 20 ]; then
+ echo "CPU IS GETTING USED MORE"
+else 
+ echo " CPU usage is normal"
 fi
 
